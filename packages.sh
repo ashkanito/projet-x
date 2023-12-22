0c363c390c0a6565384acb817677f15cec3c4727
nar308 <153825568+nar308@users.noreply.github.com>
2023-12-22 12:13:14 +0100

    Delete packages.sh

--- packages.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Install required Python packages using pip
-#pip install skyfield --target "$(pwd)/relative/path/to/installation"
-pip install skyfield --target '%cd%\relative\path\to\installation'
